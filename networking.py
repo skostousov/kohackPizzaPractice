2fc688734560b281afefb73f6df416eac2997283
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-02-20 15:41:02 -0600

    Create networking.py
    this is a class for yosef to use if he doesn't use main

---
+++ networking.py
@@ -0,0 +1,6 @@
+class coms:
+    def __init__(self):
+        pass
+    def communicate(self):
+        #put in function for now it just says pass because there is nothing in the function
+        pass
