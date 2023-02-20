fded2fb497947f447ceca056a6963aa7ba42690b
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-02-20 17:03:47 -0600

    Update hostClass.py
    this is the beginning of an algorithm to make groups now switching to personal laptop!

--- hostClass.py
+++ hostClass.py
@@ -14,11 +14,56 @@ class host:
     def commitToDB(self):
         con = sqlite3.connect("orders.db")
         cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS orders(name,address,phone,topping,slices,tracking NOT NULL PRIMARY KEY)")
-        cur.execute("INSERT INTO orders values (?,?,?,?,?,?)",self.name,self.address,self.phone,self.topping,self.slices,self.tracking)
+        cur.execute("CREATE TABLE IF NOT EXISTS orders(name,address,phone,topping,slices,tracking NOT NULL PRIMARY KEY,route)")
+        cur.execute("INSERT INTO orders (name,address,phone,topping,slices,tracking) values (?,?,?,?,?,?);",self.name,self.address,self.phone,self.topping,self.slices,self.tracking)
         con.commit()
         cur.close()
+    
+    def mapRoute(self):
+        route1 = []
+        route2 = []
+        route3 = []
+        #the function will fill these lists with all of the street names in the 3 fastest routes if there are 3 routes otherwise leave some of the lists empty
         
-  
-
+        #insert google maps code to get the routes
+        
+        #put the a list of the routes into db
+        con = sqlite3.connect("orders.db")
+        cur = con.cursor()
+        
+        cur.execute("INSERT INTO orders (route) VALUES (?);",[route1,route2,route3])
+        con.commit()
+        cur.close()
+    def chooseOrders(self):
+        #this will take the route
+        con = sqlite3.connect("orders.db")
+        cur = con.cursor()
+        cur.execute("SELECT FROM orders (tracking,slices,route) VALUES WHERE topping IS 'cheese'")
+        
+        routes=cur.fetchall()
+        #check for common roads
+        common_routes = {}
+        for route in routes:
+            #keep track of how many adsresses go through a certain road
+            for i in route:
+                if i in common_routes.keys():
+                    common_routes[i].append([route[0],route[1]]) 
+                else:
+                    common_routes[i] = [[route[0],route[1]]]
+        
+        common_streets = {}
+        #check dictionary for most used roads and make groups each group will be a list in common routes
+        for info in common_routes:
+            total_slices = 0
+            ids = []
+            for i in info:
+                total_slices += i[1]
+                ids.append(i[0])
+            common_streets[info] = [total_slices,ids]
+            
+            
+            
+                    
+            
+            
   
