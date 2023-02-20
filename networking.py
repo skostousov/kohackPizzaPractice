d3a110e3e3d9f9cc8ebe3e2932ee253447873bf8
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-02-20 17:27:19 -0600

    Update networking.py
    Yosef please create the function that is clearly denoted

--- networking.py
+++ networking.py
@@ -1,6 +1,9 @@
 class coms:
     def __init__(self):
         pass
-    def communicate(self):
-        #put in function for now it just says pass because there is nothing in the function
+    def sendToDriver(self,routelist):
+        
+        #insert function to add this routelist to something that drivers can see
+        
+        #for now it just says pass because there is nothing in the function
         pass
