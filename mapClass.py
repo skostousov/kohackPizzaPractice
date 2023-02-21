a2da3abaa8bdfe2c6f794ade34e696903da4efff
levittaharon <91753268+levittaharon@users.noreply.github.com>
2023-02-20 18:17:49 -0600

    Create mapClass.py

---
+++ mapClass.py
@@ -0,0 +1,20 @@
+class mapping:
+    def __init__(self,address,tracker):
+        self.adress = address
+        self.tracker = tracker
+
+    def mapRoute(self,tracker,address):
+        route1 = []
+        route2 = []
+        route3 = []
+        #the function will fill these lists with all of the street names in the 3 fastest routes if there are 3 routes otherwise leave some of the lists empty
+        
+        #insert google maps code to get the routes
+        
+        #put the a list of the routes into db
+        con = sqlite3.connect("orders.db")
+        cur = con.cursor()
+        
+        cur.execute("INSERT INTO orders (route) VALUES (?) WHERE tracking IS ?;",([route1,route2,route3],tracker))
+        con.commit()
+        cur.close()
